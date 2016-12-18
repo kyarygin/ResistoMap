e977c37b3b42e5a6b0473ef97952b4c284ad856b
KonstantinYarygin <yarygin@phystech.edu>
2016-12-18 21:48:57 +0300

    run script

--- preprocessing/preprocess_utils.py
+++
@@ -1,8 +0,0 @@
-from Bio import SeqIO
-import os
-
-def count_reads(readfile_path):
-    n_reads = 0
-    for record in SeqIO.parse(readfile_path, 'fastq'):
-        n_reads += 1
-    print(n_reads)
