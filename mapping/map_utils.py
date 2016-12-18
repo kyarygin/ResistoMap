08a73bb58c855b1223679dd05b82cd6846ade014
KonstantinYarygin <yarygin@phystech.edu>
2016-12-18 17:58:44 +0300

    add map_WT and map_RD funcs

--- mapping/map_utils.py
+++ mapping/map_utils.py
@@ -11,10 +11,10 @@ with open(os.path.join(SCRIPTDIR, '../config.json')) as f:
 
 
 def map_bacmet(group_name, readfile_path, n_threads, temp_folder):
-    readfile_name = os.path.basename(reads_path)
+    readfile_name = os.path.basename(readfile_path)
     sample_name, ext = os.path.splitext(readfile_name)
     daa_file_path = os.path.join(temp_folder, '{}.daa'.format(sample_name))
-    sam_file_path = os.path.join(SCRIPTDIR, '../sam/', '{}.BacMet.sam'.format(sample_name))
+    sam_file_path = os.path.join(SCRIPTDIR, '../sam/', group_name, '{}.BacMet.sam'.format(sample_name))
 
     blastx_cmd = '{diamond_path} blastx -d {index} -q {reads} -a {daa} -e 1e-5 -k 1 -p {n_threads}'
     blastx_cmd = blastx_cmd.format(
@@ -30,15 +30,56 @@ def map_bacmet(group_name, readfile_path, n_threads, temp_folder):
         diamond_path=EXEC_PATHES['diamond'],
         daa=daa_file_path,
         sam=sam_file_path
-        )
+    )
 
+    os.system(blastx_cmd)
+    os.system(view_cmd)
+    os.remove(daa_file_path)
 
+def map_RD(group_name, readfile_path, n_threads, temp_folder):
+    readfile_name = os.path.basename(readfile_path)
+    sample_name, ext = os.path.splitext(readfile_name)
+    sam_file_path = os.path.join(SCRIPTDIR, '../sam/', group_name, '{}.RD.sam'.format(sample_name))
 
+    bowtie_cmd = '{bowtie_path} -x {index} -U {reads} -S {sam} -k 1 -p {n_threads} --no-unal'
+    bowtie_cmd = bowtie_cmd.format(
+        bowtie_path=EXEC_PATHES['bowtie2'],
+        index=RD_INDEX_PATH,
+        reads=readfile_path,
+        sam=sam_file_path,
+        n_threads=n_threads
+    )
 
-# def map(group_name, read_path, n_threads):
-#     temp_folder = os.path.join(SCRIPTDIR, '../temp')
-#     os.mkdir(temp_folder)
-#     map_bacmet(group_name, read_path, n_threads, temp_folder)
+    os.system(bowtie_cmd)
+
+def map_WT(group_name, readfile_path, n_threads, temp_folder):
+    readfile_name = os.path.basename(readfile_path)
+    sample_name, ext = os.path.splitext(readfile_name)
+    sam_file_path = os.path.join(SCRIPTDIR, '../sam/', group_name, '{}.WT.sam'.format(sample_name))
+
+    bowtie_cmd = '{bowtie_path} -x {index} -U {reads} -S {sam} -k 1 -p {n_threads} --no-unal'
+    bowtie_cmd = bowtie_cmd.format(
+        bowtie_path=EXEC_PATHES['bowtie2'],
+        index=WT_INDEX_PATH,
+        reads=readfile_path,
+        sam=sam_file_path,
+        n_threads=n_threads
+    )
+
+    os.system(bowtie_cmd)
+
+def map_all_ref(group_name, read_path, n_threads):
+    temp_folder = os.path.join(SCRIPTDIR, '../temp')
+    os.mkdir(temp_folder)
+
+    group_sam_folder = os.path.join(SCRIPTDIR, '../sam/', group_name)
+    os.mkdir(group_sam_folder)
+
+    map_bacmet(group_name, read_path, n_threads, temp_folder)
+    map_RD(group_name, read_path, n_threads, temp_folder)
+    map_WT(group_name, read_path, n_threads, temp_folder)
+
+    os.rmdir(temp_folder)
 
 
 # card_index_path="$1"
