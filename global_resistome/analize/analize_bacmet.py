711aae60bc6fcfcf1597b9099edfbbce1a080828
KonstantinYarygin <yarygin@phystech.edu>
2016-12-19 20:24:07 +0300

    analize bacmet changes

--- global_resistome/analize/analize_bacmet.py
+++ global_resistome/analize/analize_bacmet.py
@@ -4,44 +4,35 @@ import pysam
 import os
 import re
 
-scriptdir = os.path.dirname(os.path.realpath(__file__))
+SCRIPTDIR = os.path.dirname(os.path.realpath(__file__))
 
-def load_bacmet_fasta(folder_path):
+def load_bacmet_fasta():
     output = defaultdict(dict)
 
-    bio_chr_path = os.path.join(folder_path, 'BacMet_EXP.BAC0000.Chromosomal.BIOCIDES.288.fasta')
-    met_chr_path = os.path.join(folder_path, 'BacMet_EXP.BAC0000.Chromosomal.METALS.283.fasta')
-    bio_pla_path = os.path.join(folder_path, 'BacMet_EXP.BAC0000.Plasmid.BIOCIDES.37.fasta')
-    met_pla_path = os.path.join(folder_path, 'BacMet_EXP.BAC0000.Plasmid.METALS.161.fasta')
+    bio_path = os.path.join(SCRIPTDIR, '../fasta/BacMet/BacMet_EXP.BIOCIDES.fasta')
+    met_path = os.path.join(SCRIPTDIR, '../fasta/BacMet/BacMet_EXP.METALS.fasta')
 
     def process_fasta(path, type_, output):
         for record in SeqIO.parse(path, 'fasta'):
             output[record.id]['set'] = type_
             output[record.id]['length'] = len(record)
 
-    process_fasta(bio_chr_path, 'Biocides', output)
-    process_fasta(met_chr_path, 'Metals', output)
-    process_fasta(bio_pla_path, 'Biocides', output)
-    process_fasta(met_pla_path, 'Metals', output)
+    process_fasta(bio_path, 'Biocides', output)
+    process_fasta(met_path, 'Metals', output)
 
     return output
 
-def parse_bacmet_sam(samfile_path, sample):
-    bacmet_data = load_bacmet_fasta(os.path.join(scriptdir, '../borya_metals/'))
-    counts = {gene_id: 0 for gene_id in bacmet_data}
+def parse_bacmet_sam(sample_name, n_reads):
+    bacmet_data = load_bacmet_fasta()
 
-    with open(os.path.join(scriptdir, '../sample_read_count.tsv')) as f:
-        data = (line.strip().split('\t') for line in f)
-        sample_sizes = {sample: int(size) for sample, size in data}
-    sample_size = sample_sizes[sample]
+    sam_file_path = os.path.join(SCRIPTDIR, '../sam/', 'BacMet.{}.sam'.format(sample_name))
 
-    with open(samfile_path) as f:
-        reader = (line.strip().split('\t') for line in f if not line.startswith('@'))
-        for record in reader:
-            gene_id = record[2]
-            counts[gene_id] += 1
+    counts = defaultdict(int)
+    for sam_record in pysam.AlignmentFile(samfile_path):
+        gene_id = sam_record.reference_name
+        counts[gene_id] += 1
 
-    rpkms = {gene_id: 1.*count/sample_size/bacmet_data[gene_id]['length'] for gene_id, count in counts.items()}
+    rpkms = {gene_id: 1.*count/n_reads/bacmet_data[gene_id]['length'] for gene_id, count in counts.items()}
     bm_rpkm_substance = {'Biocides': 0., 'Metals': 0.}
     bm_rpkm_gene = {'Biocides': defaultdict(float), 'Metals': defaultdict(float)}
     for gene_id, rpkm in rpkms.items():
