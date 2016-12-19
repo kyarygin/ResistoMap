e42129e0aae16277dfa19e82cccb631f7e97509a
KonstantinYarygin <yarygin@phystech.edu>
2016-12-19 20:24:18 +0300

    temp changes

--- global_resistome/main.py
+++ global_resistome/main.py
@@ -2,6 +2,8 @@ from global_resistome.mapping.map_utils import map_sample
 from global_resistome.utils.utils import count_reads
 
 def process_readfile(readfile_path, n_threads):
+    readfile_name = os.path.basename(readfile_path)
+    sample_name, ext = os.path.splitext(readfile_name)
     # map_sample(readfile_path, n_threads)
     n_reads = count_reads(readfile_path)
     print(n_reads)
