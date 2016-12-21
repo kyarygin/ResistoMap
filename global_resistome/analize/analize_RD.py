dada0833f1faa686e608ad1dc7da3d7f2f7bf191
KonstantinYarygin <yarygin@phystech.edu>
2016-12-21 20:59:24 +0300

    normalize names

--- global_resistome/analize/analize_RD.py
+++ global_resistome/analize/analize_RD.py
@@ -1,11 +1,12 @@
 from global_resistome.CARD_ontology.CARD_ontology import Ontology
-from global_resistome.utils.utils import short_gene_id
+from global_resistome.utils.utils import short_gene_id, normalize_name
 from collections import defaultdict
 from Bio import SeqIO
 import pysam
 import os
 
 SCRIPTDIR = os.path.dirname(os.path.realpath(__file__))
+TRASH_CATEGORIES = ['Miscellaneous', 'Mixture', 'Acridine dye']
 aro = Ontology()
 aro.initialize()
 
@@ -28,8 +29,15 @@ def analize_RD_sam(sample_name, n_reads):
     for gene_id, count in rd_read_count.items():
         length = gene_length[gene_id]
         antibiotics = aro.gene_id_to_antibiotics(gene_id)
+        antibiotics = [normalize_name(antibiotic) for antibiotic in antibiotics]
         for antibiotic in antibiotics:
             rd_arai_antibiotic[antibiotic] += 1.*count / length / n_reads
             rd_arai_gene[antibiotic][short_gene_id(gene_id)] += 1.*count / length / n_reads
+
+    for antibiotic in TRASH_CATEGORIES:
+        if antibiotic in rd_arai_gene:
+            del rd_arai_gene[antibiotic]
+        if antibiotic in rd_arai_antibiotic:
+            del rd_arai_antibiotic[antibiotic]
 
     return rd_arai_antibiotic, rd_arai_gene
