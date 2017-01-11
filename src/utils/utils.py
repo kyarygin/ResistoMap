b103535d6cdc079c6986243695108b1ac8cfd75b
KonstantinYarygin <yarygin@phystech.edu>
2017-01-11 21:58:14 +0300

    delete zero abund; gzipeed reads processing

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,12 +1,18 @@
 from Bio import SeqIO
 import pandas as pd
+import gzip
 import re
 import os
 
 def count_reads(readfile_path):
     n_reads = 0
-    for record in SeqIO.parse(readfile_path, 'fastq'):
+    if readfile_path.endswith('.gz'):
+        handle = gzip.open(readfile_path)
+    else:
+        handle = open(readfile_path)
+    for record in SeqIO.parse(handle, 'fastq'):
         n_reads += 1
+    handle.close()
     return n_reads
 
 def short_gene_id(gene_id):
@@ -20,6 +26,7 @@ def substance_dict_to_table(rpkm_substance, sample_name):
     df = pd.DataFrame(data, columns=['category', 'abund'])
     df['sample'] = sample_name
     df = df[['category', 'sample', 'abund']]
+    df = df.ix[df['abund'] != 0]
     return df
 
 def gene_dict_to_table(rpkm_gene, sample_name):
@@ -27,6 +34,7 @@ def gene_dict_to_table(rpkm_gene, sample_name):
     df = pd.DataFrame(data, columns=['category', 'gene id', 'abund'])
     df['sample'] = sample_name
     df = df[['category', 'gene id', 'sample', 'abund']]
+    df = df.ix[df['abund'] != 0]
     return df
 
 def delete_folder(top):
